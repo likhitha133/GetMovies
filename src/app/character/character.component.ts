c00a709afefad0e21e2c8ed594a82c1719aebb91
likhitha133 <51676764+likhitha133@users.noreply.github.com>
2019-06-18 15:41:49 -0400

    Update character.component.ts

--- src/app/character/character.component.ts
+++ src/app/character/character.component.ts
@@ -3,63 +3,80 @@ import { CharacterService } from './../../services/character.service';
 import { Character, Characters } from '../models/character';
 import { Profile } from '../models/profile';
 import { MovieDetail } from './../models/movieDetails';
+import { Observable, observable } from 'rxjs';
 
 @Component({
   selector: 'app-character',
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.css']
-  
+
 })
 export class CharacterComponent implements OnInit {
 
-  public characters: Array<Character>;
-  public profile: Profile;
-  public movieUrl: Array<string>;
-  public movieNames: Array<MovieDetail>;
-  public showMovie: Boolean;
+  characters: Array<Character>;
+  profile: Profile;
+  movieUrl: string[] = [];
+  movieNames = [];
+  showMovie = false;
   errorResponse: any;
   errorMessage: string;
-  showError: boolean;
+  showError = false;
   selectedCharacter: Character;
-
-  constructor(private _charService: CharacterService) {
-    this.movieNames = [];
-    this.showMovie = false;
-    this.movieUrl = [];
-    this.showError = false;
-   }
-//service is getting data from API/json file
-  ngOnInit() {
-    this._charService.getCharacters().subscribe((res: Characters) => {
-        this.characters = res.characters;
-    });
-  }  
-
-  getMovies(char: Character) {
-    this.movieNames = [];
-    this.selectedCharacter = char;
-    this._charService.getMovies(char).subscribe((res: Profile)=> {
-      this.showMovie = true;
-      this.showError = false;
-      this.profile = res;
-      this.movieUrl = this.profile.films;
-      this.getMovieNames();
-    }, error => {
-      this.errorMessage = error.error.detail;
-      this.showError = true;
-    });
-  }
   
 
+
+  constructor(private _charService: CharacterService) {
+    
+  }
+
+  ngOnInit() {
+    this._charService.getCharacters().subscribe((res: Characters) => {
+      this.characters = res.characters;
+    });
+  }
+
+
+  getMovies(char: Character) {
+    this.movieNames = []; 
+    this.selectedCharacter = char;
+    this._charService.getMovies(char)
+      .subscribe((res: Profile) => {
+        this.showMovie = true;
+        this.showError = false;
+        this.profile = res; 
+        this.movieUrl = this.profile.films; 
+        this.getMovieNames();
+      },
+        error => this.errorHandling(error.error.detail)
+      );
+  }
+
+
+  // for(let i = 0; i < this.movieUrl.length; i++) {
+  //   this._charService.getMovieName(this.movieUrl[i]).subscribe((res: MovieDetail)=> {
+  //     this.movieNames.push(res);
+  //   }, error => {
+  //     this.errorMessage = error.error.detail;
+  //     this.showError = true;
+  //   });
+  // }
+
+
+
+
   getMovieNames() {
-    for(let i = 0; i < this.movieUrl.length; i++) {
-      this._charService.getMovieName(this.movieUrl[i]).subscribe((res: MovieDetail)=> {
-        this.movieNames.push(res);
-      }, error => {
-        this.errorMessage = error.error.detail;
-        this.showError = true;
-      });
-    }
+    this.movieUrl.map(movie =>
+      this._charService.getMovieName(movie)
+        .subscribe(
+          detail => this.movieNames.push(detail),
+          error => this.errorHandling(error.error.detail)
+        )
+    )
+  };
+
+  errorHandling(message: string) {
+    this.errorMessage = message;
+    this.showError = true;
   }
 
 }
